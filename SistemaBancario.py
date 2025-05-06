d17d0bc505a8aab1ad54784fd7c542dcb3989bd0
Cauan Albuquerque <cauan.allbuquerque@gmail.com>
2025-05-06 16:16:59 -0300

    adicionando funções

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -1,63 +1,81 @@
-menu = """
-======================//======================
-Seja bem vindo(a) ao banco digital!
-Qual operação deseja executar?
+def menu():
+    return """
+    ======================//======================
+    Seja bem vindo(a) ao banco digital!
+    Qual operação deseja executar?
 
-[d] depositar
-[s] sacar
-[e] Extrato
-[q] Sair
-======================//======================
+    [d] depositar
+    [s] sacar
+    [e] Extrato
+    [q] Sair
+    ======================//======================
+    """
 
-"""
-
-
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-
-while True:
-    opcao = input(menu)
+def depositar(saldo, valor, extrato):
+    saldo += valor
+    extrato += f"deposito de R${valor}\n"
+    return saldo, extrato
     
-    
-    if opcao == "d":
-        deposito = float(input("Digite o valor que deseja depositar: "))
-        saldo =+ deposito
-        print(f"Operação bem sucedida, seu novo saldo é de: {saldo}")
-        extrato += f"deposito de R${deposito}\n"
-        
-        
-    elif opcao == "s":
-        saque = float(input("Digite o valor que deseja sacar: "))
-        if saque > saldo:
-            print("Você não tem dinheiro suficiente na conta! ")
 
-        else: 
-            if saque > 500:
-                print(f"O máximo que se pode sacar em uma operação é: {limite}")
+def sacar(saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
+    if valor > saldo:
+        return "Você não tem dinheiro suficiente na conta! "
+
+    else: 
+        if valor > limite:
+            return f"O máximo que se pode sacar em uma operação é: {limite}, tente novamente!"
                 
-            else:
-                saldo =- saque
-                print("Saque efetuado")           
-                extrato += f"Saque de R${saque}\n"
+        else:
+            saldo -= valor
+            print("Saque efetuado")           
+            extrato += f"Saque de R${valor}\n"
+            numero_saques += 1
+            return saldo, extrato, numero_saques
+
+    
+def exibir_extrato(saldo, extrato):
+    print(extrato if extrato else "Nenhuma movimentação realizada.")
+    print(f"Saldo atual: R${saldo:.2f}")
             
-    elif opcao == "e":
-        print(extrato)
+
+def main(): 
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+
+    while True:
+        opcao = input(menu())
     
+    
+        if opcao == "d":
+            valor = float(input("Digite o valor que deseja depositar: "))
+            saldo, extrato = depositar(saldo, valor, extrato)
         
-    elif opcao == "q":
-        break
-    
-    
-    else:
-        print("Digite uma opção válida!")
-    
-    
-    
-    print(f"Seu saldo é de: {saldo}")
-    
-    if numero_saques == LIMITE_SAQUES:
-        print("Limites de saque atingido! ")
-    
+        
+        elif opcao == "s":
+            valor = float(input("Digite o valor que deseja sacar: "))
+            saldo, extrato, numero_saques = sacar(saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES)
+            if numero_saques == LIMITE_SAQUES:
+                break
+            print("Saque efetuado! ")
+            
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato)
+        
+            
+        elif opcao == "q":
+            break
+        
+        
+        else:
+            print("Digite uma opção válida!")
+        
+        
+        
+        print(f"Seu saldo é de: {saldo}")
+        
+
+main()
