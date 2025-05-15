c7995356a1d4a340fc59da570d4d694fe480cf01
Cauan Albuquerque <cauan.allbuquerque@gmail.com>
2025-05-15 15:56:13 -0300

    Daily commit

--- teste.py
+++ teste.py
@@ -1,2 +1,22 @@
-frutas = ["banana", "morango"]
-print(frutas[0])
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self._preco = preco
+        
+    @property 
+    def preco(self):
+        return self._preco
+    
+    @preco.setter
+    def preco(self, valor):
+        if valor > 0:
+            self._preco = valor
+            
+        else:
+            print("Preço inválido!")
+            
+produto = Produto("Mousse", 150)
+print(produto.preco)
+
+produto.preco = -50
+print(produto.preco)
